a2918cceaa1332cae940ee8b0834a6bb23d4ebed
ka-suratcha <ka.suratcha@gmail.com>
2024-04-16 02:56:57 +0700

    POST method
    - create POST method with sented body then write file
    - file-based, find id for new data and push data and new id to obj

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // rn file-based API (read data from file) later store data in DB
+// status code 200 completed, 201 created
 
 const fs = require('fs');
 // all express config in app.js
@@ -7,12 +8,18 @@ const express = require('express');
 // add express method to app
 const app = express();
 
+// MIDDLEWARE
+// in the middle of req and res (the step go through)
+// modify the incoming req data before res
+app.use(express.json()); // data from body is added req obj
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 //ROUNTING determine how app res to certain client req and URL
 // method, URL root (endpoint), status code, resource
 // good to specify the API ver (in case changes -> without breaking who still use old ver) [branch off]
-// GET method is sent to server on this URL
+
+// GET method -> get data
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({  // JSON formatiing standard
     status: 'success',
@@ -22,6 +29,28 @@ app.get('/api/v1/tours', (req, res) => {
     },
   })
 });
+
+// POST method -> create new data
+// send data from client to server, data available on req -> req hold all data
+// express doesnt put body data on req -> use middleware
+app.post('/api/v1/tours/', (req, res) => {
+  console.log(req.body);
+
+  // find id to create new data (usually db takes care of this)
+  const newId = tours[tours.length - 1].id + 1 // length get num, -1 for get lastest index
+  const newTour = Object.assign({id: newId}, req.body); // merging 2 objects
+  tours.push(newTour); // push new tour to tours obj
+
+  // use async ver cuz rn we in callback func
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => { // overwrite
+    res.status(201).json({
+      status: "success",
+      data:{
+        tour: newTour
+      }
+    })
+  })
+})
 
 // START SERVER
 const port = 3000
