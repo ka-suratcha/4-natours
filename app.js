b7171e1673e95da0456af530fdede6565d4c4f2b
ka-suratcha <ka.suratcha@gmail.com>
2024-04-16 23:28:56 +0700

    "user" routes
    - create routes for users

--- app.js
+++ app.js
@@ -128,28 +128,42 @@ const deleteTour = (req, res) => {
   });
 };
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yer deined'
+  })
+}
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yer deined'
+  })
+}
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yer deined'
+  })
+}
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yer deined'
+  })
+}
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yer deined'
+  })
+}
+
 // 3.) ROUNTING determine how app res to certain client req and URL
 // method, URL root (endpoint), status code, resource
 // good to specify the API ver (in case changes -> without breaking who still use old ver) [branch off]
 
-// GET method -> get data
-app.get('/api/v1/tours', getAllTours);
-
-// api with passing variable
-app.get('/api/v1/tours/:id', getTour);
-
-// POST method -> create new data
-// send data from client to server, data available on req -> req hold all data
-
-app.post('/api/v1/tours', createTour);
-
-// PATCH method update data (only expect properties)
-app.patch('/api/v1/tours/:id', updateTour);
-
-// DETELE method delete data
-app.delete('/api/v1/tours/:id', deleteTour);
-
-// in case change ver or resource name
+// TOUR
 app
   .route('/api/v1/tours')
   .get(getAllTours)
@@ -158,8 +172,20 @@ app
 app
   .route('/api/v1/tours/:id')
   .get(getTour)
-  .patch(createTour)
+  .patch(updateTour)
   .delete(deleteTour);
+
+// USER
+app
+  .route('/api/v1/users')
+  .get(getAllUsers)
+  .post(createUser);
+
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 // 4.) START SERVER
 const port = 3000;
