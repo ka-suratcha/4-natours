1bdd53606ea13dda02097b53c4d0d1fd6b7bee89
ka-suratcha <ka.suratcha@gmail.com>
2024-04-16 20:20:07 +0700

    reorganize route (handler)
    - all route should kind together and handler func also (refactor code)
    - export a handler func into their own func

--- app.js
+++ app.js
@@ -15,34 +15,29 @@ app.use(express.json()); // data from body is added req obj
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-//ROUNTING determine how app res to certain client req and URL
-// method, URL root (endpoint), status code, resource
-// good to specify the API ver (in case changes -> without breaking who still use old ver) [branch off]
-
-// GET method -> get data
-app.get('/api/v1/tours', (req, res) => {
+// EXPORT route handler
+const getAllTours = (req, res) => {
   res.status(200).json({  // JSON formatiing standard
     status: 'success',
     results: tours.length, // num of result (tours is JS object)
     data: { // envelope for data
       tours: tours
     }
-  })
-});
+  });
+};
 
-// api with passing variable
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   console.log(req.params);
 
   // find el where id = one that get from params (URL)
   const id = req.params.id * 1;
-  const tour = tours.find(el => el.id === id)
+  const tour = tours.find(el => el.id === id);
 
   if (!tour) {
     return res.status(404).json({
       status: 'failed',
       message: 'invaild ID'
-    })
+    });
   }
 
   res.status(200).json({  // JSON formatiing standard
@@ -50,17 +45,14 @@ app.get('/api/v1/tours/:id', (req, res) => {
     data: { // envelope for data
       tours: tour
     }
-  })
-});
+  });
+};
 
-// POST method -> create new data
-// send data from client to server, data available on req -> req hold all data
-// express doesnt put body data on req -> use middleware
-app.post('/api/v1/tours/', (req, res) => {
+const createTour = (req, res) => {
   console.log(req.body);
 
   // find id to create new data (usually db takes care of this)
-  const newId = tours[tours.length - 1].id + 1 // length get num, -1 for get lastest index
+  const newId = tours[tours.length - 1].id + 1; // length get num, -1 for get lastest index
   const newTour = Object.assign({ id: newId }, req.body); // merging 2 objects
   tours.push(newTour); // push new tour to tours obj
 
@@ -71,72 +63,71 @@ app.post('/api/v1/tours/', (req, res) => {
       data: {
         tour: newTour
       }
-    })
-  })
-})
+    });
+  });
+};
 
-// PATCH method update data (only expect properties)
-app.patch('/api/v1/tours/:id', (req, res) => {
-
+const updateTour = (req, res) => {
   // find el where id = one that get from params (URL)
   const id = req.params.id * 1;
-  const tour = tours.find(el => el.id === id)
+  const tour = tours.find(el => el.id === id);
 
   if (!tour) {
     return res.status(404).json({
       status: 'failed',
       message: 'invaild ID'
-    })
+    });
   }
 
   res.status(200).json({
     status: 'success',
     data: '<update tour>'
-  })
-})
+  });
+};
 
-// PATCH method update data (only expect properties)
-app.patch('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
 
   // find el where id = one that get from params (URL)
   const id = req.params.id * 1;
-  const tour = tours.find(el => el.id === id)
+  const tour = tours.find(el => el.id === id);
 
   if (!tour) {
     return res.status(404).json({
       status: 'failed',
       message: 'invaild ID'
-    })
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: '<update tour>'
-  })
-})
-
-// DETELE method delete data
-app.delete('/api/v1/tours/:id', (req, res) => {
-
-  // find el where id = one that get from params (URL)
-  const id = req.params.id * 1;
-  const tour = tours.find(el => el.id === id)
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'failed',
-      message: 'invaild ID'
-    })
+    });
   }
 
   res.status(204).json({
     status: 'success',
     data: null
-  })
-})
+  });
+};
+
+//ROUNTING determine how app res to certain client req and URL
+// method, URL root (endpoint), status code, resource
+// good to specify the API ver (in case changes -> without breaking who still use old ver) [branch off]
+
+// GET method -> get data
+app.get('/api/v1/tours', getAllTours);
+
+// api with passing variable
+app.get('/api/v1/tours/:id', getTour);
+
+// POST method -> create new data
+// send data from client to server, data available on req -> req hold all data
+// express doesnt put body data on req -> use middleware
+app.post('/api/v1/tours/', createTour);
+
+// PATCH method update data (only expect properties)
+app.patch('/api/v1/tours/:id', updateTour);
+
+// DETELE method delete data
+app.delete('/api/v1/tours/:id', deleteTour);
+
 
 // START SERVER
-const port = 3000
+const port = 3000;
 // listening to req
 app.listen(port, () => {
   console.log(`app running on port ${port}...`);
