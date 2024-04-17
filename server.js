adc1f85e53c485c26fb5aab73016ed5094c7fda3
ka-suratcha <ka.suratcha@gmail.com>
2024-04-17 10:31:55 +0700

    connect to db
    - connect to mongoDB
    - use env var to connect db

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 // import app or other related to our app
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
-dotenv.config({ patch: './config.env' });
+dotenv.config({ path: './config.env' });
 
 const app = require('./app.js');
+
+dotenv.config({ patch: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection successfully');
+  });
 
 // ENVIRONMENT
 console.log(`Environment: ${app.get('env')}`);
