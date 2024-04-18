e950ae745ac9d4c9915919b5d97e091fd984bd05
ka-suratcha <ka.suratcha@gmail.com>
2024-04-18 10:07:32 +0700

    implement page and limit fields
    - page -> allowing the user to only select a certain page of out result
    - limt -> limt per page

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,7 +6,8 @@ const Tour = require('./../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     // == BUILD QUERY
-    // == 1A.) Filltering
+
+    // ==== 1A.) Filltering
     // have to do hard copy cuz JS, when set a var in to another obj, that new var is a reference to that original obj
     const queryObj = { ...req.query }; // shallow copy of req.query, create obj out of that
     const excludedFields = ['page', 'sort', 'limit', 'fields']; // create array of all fields that want to exculde
@@ -14,73 +15,94 @@ exports.getAllTours = async (req, res) => {
     // remove fields from query obj by loop over
     excludedFields.forEach((el) => delete queryObj[el]); // delete the field with the name of el
 
-    // == 1B.) ADV FILTERRING
+    // ==== 1B.) ADV FILTERRING
     // MongonDB -> { el { '$lt': 'num' } }
     let queryStr = JSON.stringify(queryObj); // JS Obj to JSON String, let cuz edit
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`); // reaplce $ in front of these -> for MongoDB
 
+    // config query
     let query = Tour.find(JSON.parse(queryStr)); // no way later to implementing other feature -> save Tour.find(query)
 
-    // query request
-    console.log('\n=== Filltering ===');
-    console.log('\nreq.query:');
-    console.log(req.query);
+    console.log(
+      '\n========== Filltering ==========\n- req.query:',
+      req.query,
+      '\n\n- req.query that exculded:\t\t\t\t',
+      queryObj,
+      '\n- req.query that exculded and add $ (Filttering):\t',
+      queryStr,
+      '\n'
+    );
 
-    // query that exculded
-    console.log('\nreq.query that exculded:');
-    console.log(queryObj);
-
-    // query that has finished formatting of MongoDB to understand
-    console.log('\nreq.query that exculded and add $ (Filttering):');
-    console.log(JSON.parse(queryStr));
-
-    // == 2.) Sorting
+    // ==== 2.) Sorting
     if (req.query.sort) {
       // Mongoose -> sort(el1 el2)
       const sortBy = req.query.sort.split(',').join(' '); //split String with comma -> array of all the string
 
+      // config query
       query = query.sort(sortBy);
 
       // query request from sort
-      console.log('\n=== Sorting ===');
-      console.log('\nreq.query.sort:');
-      console.log(req.query.sort);
-
-      // get each var of query as array
-      console.log('\nreq.query.sort.with.split:');
-      console.log(req.query.sort.split(','));
-
-      // get each var of query as array then join it tgt make it become String of Mongoose to understand
-      console.log('\nreq.query.sort.split.join (sorting):');
-      console.log(req.query.sort.split(',').join(' '));
+      console.log(
+        '\n========== Sorting ==========\n- req.query.sort:\t\t\t',
+        req.query.sort,
+        '\n- req.query.sort.with.split:\t\t',
+        req.query.sort.split(','),
+        '\n- req.query.sort.split.join (Sorting):\t',
+        req.query.sort.split(',').join(' '),
+        '\n'
+      );
     } else {
       // default
-      query = query.sort('-createdAt');
+      // query = query.sort('-__id');
     }
 
-    // == 3.) Field limiting
+    // ==== 3.) Field limiting
     if (req.query.fields) {
       // Mongoose -> sort(el1 el2)
       const fields = req.query.fields.split(',').join(' ');
 
+      // config query
       query = query.select(fields);
 
-      // query request from sort
-      console.log('\n=== Field limiting ===');
-      console.log('\nreq.query.fields:');
-      console.log(req.query.fields);
-
-      // get each var of query as array
-      console.log('\nreq.query.fields.with.split:');
-      console.log(req.query.fields.split(','));
-
-      // get each var of query as array then join it tgt make it become String of Mongoose to understand
-      console.log('\nreq.query.fields.split.join (fieldsing):');
-      console.log(req.query.fields.split(',').join(' '));
-      
+      console.log(
+        '\n========== Field limiting ==========\nreq.query.fields:\t\t\t\t',
+        req.query.fields,
+        '\nreq.query.fields.with.split\t\t\t',
+        req.query.fields.split(','),
+        '\nreq.query.fields.split.join (Fileding):\t\t',
+        req.query.fields.split(',').join(' '),
+        '\n'
+      );
     } else {
       query = query.select('-__v'); // - is exculding
     }
+
+    // ==== 4.) Pagination
+    // convert to number or use default
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+
+    // get page before and * with limit per page to get skip
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    // in case skip more doc than actually have
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page dose note exist');
+    }
+
+    console.log(
+      '\n========== Pagination ==========\npage:\t',
+      req.query.page * 1 || 1,
+      '\nlimit:\t',
+      req.query.limit * 1 || 100,
+      '\nskip:\t',
+      (page - 1) * limit,
+      '\n'
+    );
+
     // == EXECUTE QUERY
     const tours = await query; // find with query that already exculde field
 
