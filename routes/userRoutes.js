1d4daf84e1c9ca053635a218a7073220168af94e
ka-suratcha <ka.suratcha@gmail.com>
2024-04-22 17:16:31 +0700

    create new user
    - implement API to create new user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,12 +2,15 @@
 const express = require('express');
 
 // REQ OWN MOUDLE
+const authController = require('./../controllers/authController');
 const userController = require('./../controllers/userController');
 
 // MIDDLEWARE
 const router = express.Router();
 
 // USER
+router.post('/signup', authController.signup);
+
 router
   .route('/')
   .get(userController.getAllUsers)
