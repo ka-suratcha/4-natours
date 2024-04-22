5a2b8bd0edc1c60a468741a1d4c3a565496783d1
ka-suratcha <ka.suratcha@gmail.com>
2024-04-22 15:54:21 +0700

    create catch uncaught expections
    - uncaught exception -> error or bug that are not handled anywhere

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const app = require('./app.js');
 
 // == CONNECT TO DB
