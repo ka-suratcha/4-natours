7e46a82355690cca460a3f2e6470abd6af24814b
ka-suratcha <ka.suratcha@gmail.com>
2024-04-22 17:54:49 +0700

    manage users password in DB
    - implement validation of password and passwordConfirm
    - never store plain passwords in DB -> encrypt password (hash)
    - undefined passwordConfrim -> only need when input

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -8,10 +9,10 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-    required: [true, 'Please tell us your email!'],
+    required: [true, 'Please provide your email'],
     unique: true,
     lowercase: true,
-    validator: [validator.isEmail, 'Please provide a valid email'],
+    validate: [validator.isEmail, 'Please provide a valid email'],
   },
   photo: String,
   password: {
@@ -22,7 +23,27 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm password'],
+    validate: {
+      // only work on CREATE and SAVE!!!! (update not work)
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Password are not the same!',
+    },
   },
+});
+
+// encryption before store in DB
+userSchema.pre('save', async function (next) {
+  // only run this func if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
